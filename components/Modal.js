7a6a92d5c50ef27d9a1a7f894aa77bcb90e595c7
Ali <87544521+lukef7fywmrp@users.noreply.github.com>
2022-01-10 04:31:47 +0400

    modified modal position

--- components/Modal.js
+++ components/Modal.js
@@ -62,7 +62,7 @@ const Modal = ({ handleClose, type }) => {
       {type === "dropIn" && (
         <motion.div
           onClick={(e) => e.stopPropagation()}
-          className="rounded-xl flex flex-col justify-center bg-white dark:bg-[#1D2226] w-full max-w-lg -mt-36 md:-mt-96 mx-6"
+          className="rounded-xl flex flex-col justify-center bg-white dark:bg-[#1D2226] w-full max-w-lg md:-mt-96 mx-6"
           variants={dropIn}
           initial="hidden"
           animate="visible"
